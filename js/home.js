4adb0bb8105327ab9a9b1b60471d6186790adaaa
bloompace <bloomspaceweb@gmail.com>
2025-04-27 11:04:49 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '1a3490d432e1147cdbb5fa502180e930';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
